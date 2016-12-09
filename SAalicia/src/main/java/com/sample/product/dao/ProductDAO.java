fc2d61cbc4dc0774a73c7ac46d883a5a78a6d722
ALICIA <ALICIA@140.136.18.41>
2016-12-09 17:08:26 +0800

    2016/12/9/17:08

---
+++ SAalicia/src/main/java/com/sample/product/dao/ProductDAO.java
@@ -0,0 +1,26 @@
+package com.sample.product.dao;
+
+import java.util.List;
+
+import com.sample.product.entity.Product;
+
+public interface ProductDAO {
+	
+	public List<Product> getList();
+	public List<Product> getListArrival(String c);//category產品類別
+	public List<Product> getListSale(String c);
+	public List<Product> getListWomen(String c);
+	public List<Product> getListAccessorizies(String c);
+	
+	public void insert(Product aProduct);
+	
+	public Product get(long id);
+	
+	public void update(Product aProduct);
+	
+	public void delete(long id);
+
+	public int count();
+
+}//ProductDAO
+
